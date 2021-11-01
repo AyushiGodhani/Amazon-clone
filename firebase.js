b6d311a40c37a09dbff0df0ff0617d1ca4f41053
AyushiGodhani <92893600+AyushiGodhani@users.noreply.github.com>
2021-11-01 17:17:32 -0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDEsPXb3YeBYex2XGdp6SX0ViCg3YKGkLQ",
+    authDomain: "challenge-b1643.firebaseapp.com",
+    projectId: "challenge-b1643",
+    storageBucket: "challenge-b1643.appspot.com",
+    messagingSenderId: "290337271899",
+    appId: "1:290337271899:web:a0f3e2cbe4fee72308cf7e",
+    measurementId: "G-YKZD0XMDBM"
+  };
